b966dffc8cf9194d6b149fde7e4793c20f98decc
chandra88 <csah88@yahoo.com>
2014-07-01 18:19:12 -0400

    first commit

---
+++ BOSClass/CMakeFiles/BOSClass.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+FILE(REMOVE_RECURSE
+  "HEADDict.cxx"
+  "HEADDict.h"
+  "TAGRDict.cxx"
+  "TAGRDict.h"
+  "MCTKDict.cxx"
+  "MCTKDict.h"
+  "MCVXDict.cxx"
+  "MCVXDict.h"
+  "CMakeFiles/BOSClass.dir/HEAD.cxx.o"
+  "CMakeFiles/BOSClass.dir/HEADDict.cxx.o"
+  "CMakeFiles/BOSClass.dir/TAGR.cxx.o"
+  "CMakeFiles/BOSClass.dir/TAGRDict.cxx.o"
+  "CMakeFiles/BOSClass.dir/MCTK.cxx.o"
+  "CMakeFiles/BOSClass.dir/MCTKDict.cxx.o"
+  "CMakeFiles/BOSClass.dir/MCVX.cxx.o"
+  "CMakeFiles/BOSClass.dir/MCVXDict.cxx.o"
+  "libBOSClass.pdb"
+  "libBOSClass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/BOSClass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
